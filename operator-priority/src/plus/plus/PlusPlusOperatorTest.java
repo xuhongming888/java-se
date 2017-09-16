cf42d18a26facdfbd0973ecc721920a4a11326df
xuhongming888 <xuhongming888@gmail.com>
2017-09-16 14:47:03 +0800

    操作符优先权

---
+++ operator-priority/src/plus/plus/PlusPlusOperatorTest.java
@@ -0,0 +1,15 @@
+package plus.plus;
+
+public class PlusPlusOperatorTest {
+
+	public static void main(String[] args) {
+		
+		int j = 0;
+		j = j++ + ++j + j++ + ++j;
+		System.out.println("j = " + j);
+		
+	}
+	
+	
+	
+}
